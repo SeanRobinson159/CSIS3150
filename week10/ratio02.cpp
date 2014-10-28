be209c37a9cf03bff152aeac9e15bd7aabe3fa61
SeanRobinson159 <SeanRobinson159@gmail.com>
2014-10-28 16:58:42 -0600

    Classwork 10-28
    We started working on methods and classes for C++. We worked on the
    ratio class (1-3) We also decided what we wanted to do for our final
    group project.

---
+++ week10/ratio02.cpp
@@ -0,0 +1,45 @@
+// A Ratio class
+
+#include <iostream>
+using namespace std;
+
+class Ratio {
+  	public:
+        Ratio(int n, int d);
+        double convert();
+  		void invert();
+  		void print();
+  	private:
+  		int num;
+  		int den;
+};
+  
+  int main() {
+    Ratio x(22,7);
+    cout << "x = ";
+    x.print();
+    cout << " = " << x.convert() << endl;
+    x.invert();
+    cout << "1/x = "; x.print();
+    cout << endl;
+  }
+  
+Ratio::Ratio(int n, int d){
+   		num = n;
+    	den = d;
+  	}
+
+	double Ratio::convert() {
+     	return double(num)/den; 
+    }
+
+	void Ratio::invert(){
+     	int temp = num;
+      	num = den;
+      	den = temp;
+    }
+
+	void Ratio::print(){
+ 		cout << num << '/' << den; 
+	}
+  
