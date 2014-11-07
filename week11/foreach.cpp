78ca59767942bffa9948a9abd32af0af9824d382
SeanRobinson159 <SeanRobinson159@gmail.com>
2014-11-06 17:32:49 -0700

    Classwork

---
+++ week11/foreach.cpp
@@ -0,0 +1,17 @@
+//foreach
+
+#include <iostream>
+#include <list>
+#include <string>
+using namespace std;
+
+void cube (int n);
+
+int main() {
+    int a[] = {0,1,2,3,4,5,6,7,8,9,10};
+    for_each(a, a+10, cube);
+}
+
+void cube (int n){
+    cout << n*n*n << endl;
+}
